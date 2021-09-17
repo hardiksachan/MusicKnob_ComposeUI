7cadd29bdaa6b513a18bb8346952a8bec3f92dd3
Hardik Sachan <sachanhardik@gmail.com>
2021-09-17 19:27:00 +0530

    Created Music Knob and Volume Bar

--- app/src/main/java/com/hardiksachan/musicknob_composeui/MainActivity.kt
+++ app/src/main/java/com/hardiksachan/musicknob_composeui/MainActivity.kt
@@ -1,22 +1,163 @@
 package com.hardiksachan.musicknob_composeui
 
 import android.os.Bundle
+import android.view.MotionEvent
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInteropFilter
+import androidx.compose.ui.layout.boundsInWindow
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import com.hardiksachan.musicknob_composeui.ui.theme.MusicKnob_ComposeUITheme
+import kotlin.math.PI
+import kotlin.math.atan2
+import kotlin.math.roundToInt
 
+
+val dotColor = Color(0xFFFF5757)
+
+@ExperimentalComposeUiApi
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MusicKnob_ComposeUITheme {
-
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Row(
+                        horizontalArrangement = Arrangement.Center,
+                        verticalAlignment = Alignment.CenterVertically,
+                        modifier = Modifier
+                            .border(1.dp, dotColor, RoundedCornerShape(10.dp))
+                            .padding(32.dp)
+                    ) {
+                        var volume by remember {
+                            mutableStateOf(0f)
+                        }
+                        val barCount = 30
+                        MusicKnob(modifier = Modifier.size(100.dp)) { volume = it }
+                        Spacer(Modifier.width(24.dp))
+                        VolumeBar(
+                            Modifier
+                                .fillMaxWidth()
+                                .height(32.dp),
+                            activeBars = (volume * barCount).roundToInt(),
+                            totalBars = barCount
+                        )
+                    }
+                }
             }
         }
     }
 }
+
+@Composable
+fun VolumeBar(
+    modifier: Modifier = Modifier,
+    activeBars: Int = 0,
+    totalBars: Int = 10,
+) {
+    BoxWithConstraints(
+        modifier = modifier,
+        contentAlignment = Alignment.Center
+    ) {
+        val barWidth = remember {
+            constraints.maxWidth / (2f * totalBars)
+        }
+
+        Canvas(modifier = modifier) {
+            for (i in 0 until totalBars) {
+                drawRoundRect(
+                    color = if (i in 0..activeBars) dotColor else Color.Gray,
+                    topLeft = Offset(i * barWidth * 2f + barWidth / 2f, 0f),
+                    size = Size(barWidth, constraints.maxHeight.toFloat()),
+                    cornerRadius = CornerRadius.Zero
+                )
+            }
+        }
+    }
+}
+
+@ExperimentalComposeUiApi
+@Composable
+fun MusicKnob(
+    modifier: Modifier = Modifier,
+    limitingAngle: Float = 25f,
+    onValueChanged: (Float) -> Unit
+) {
+    var rotation by remember {
+        mutableStateOf(limitingAngle)
+    }
+
+    var touchX by remember {
+        mutableStateOf(0f)
+    }
+    var touchY by remember {
+        mutableStateOf(0f)
+    }
+    var centerX by remember {
+        mutableStateOf(0f)
+    }
+    var centerY by remember {
+        mutableStateOf(0f)
+    }
+
+    Image(
+        painter = painterResource(id = R.drawable.music_knob),
+        contentDescription = "Music Knob",
+        modifier = modifier
+            .fillMaxSize()
+            .onGloballyPositioned {
+                val windowBounds = it.boundsInWindow()
+                centerX = windowBounds.size.width / 2f
+                centerY = windowBounds.size.height / 2f
+            }
+            .pointerInteropFilter { event ->
+                touchX = event.x
+                touchY = event.y
+
+                val angle = -atan2(centerX - touchX, centerY - touchY) * (180f / PI).toFloat()
+
+                when (event.action) {
+                    MotionEvent.ACTION_DOWN,
+                    MotionEvent.ACTION_MOVE -> {
+                        if (angle !in -limitingAngle..limitingAngle) {
+
+                            val absoluteAngle =
+                                if (angle < 0) 360f + angle else angle
+
+                            rotation = absoluteAngle
+
+                            val percent =
+                                (absoluteAngle - limitingAngle) / (360f - 2 * limitingAngle)
+                            onValueChanged(percent)
+
+                            true
+                        } else {
+                            false
+                        }
+                    }
+                    else -> false
+                }
+            }
+            .rotate(rotation)
+    )
+}
+ 
